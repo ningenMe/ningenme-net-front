a4743a9c1f7c63a713996ffb0a6765295b913576
ningenMe <amohkuru@gmail.com>
2020-05-04 03:06:16 +0900

    add header and footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import React  from 'react';
+import Header from "./js/components/Header";
+import Footer from "./js/components/Footer";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload hoge.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Footer />
     </div>
   );
 }
