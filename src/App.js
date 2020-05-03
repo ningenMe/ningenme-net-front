bf751c77454ddffe473183f23cca18a671700c64
ningenMe <amohkuru@gmail.com>
2020-05-04 03:30:26 +0900

    add router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React  from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import Header from "./js/components/Header";
 import Footer from "./js/components/Footer";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+          <Route exact path='/' component={Index} />
+          <Route path='/second' component={Second} />
+          <Route path='/third' component={Third} />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
+
+const Index = () => (
+  <div>
+    <h2>Index</h2>
+    <p>インデックスページ</p>
+  </div>
+)
+const Second = () => (
+  <div>
+    <h2>Second</h2>
+    <p>二番目のページです</p>
+  </div>
+)
+const Third = () => (
+  <div>
+    <h2>Third</h2>
+    <p>三番目のページです</p>
+  </div>
+)
 
 export default App;
