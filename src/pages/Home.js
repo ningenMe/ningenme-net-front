e1312967b2972125b6b54db3c3818739a0b08976
ningenMe <amohkuru@gmail.com>
2020-05-07 22:16:58 +0900

    add contents

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,20 @@
 import React  from 'react';
+import TextCard from '../components/TextCard';
 import LinkListCard from '../components/LinkListCard';
+import TextListCard from '../components/TextListCard';
 import * as Contents from '../assets/Contents';
 
 export default class Home extends React.Component {
     render() {
         return (
-            //Applications
+            <div>
+            <TextCard title="Profile" items={Contents.Profile}></TextCard>
             <LinkListCard title="Application" items={Contents.Applications}></LinkListCard>
+            <LinkListCard title="Problem" items={Contents.Problems}></LinkListCard>
+            <TextListCard title="Activitiy" items={Contents.Activities}></TextListCard>
+            <LinkListCard title="Paper" items={Contents.Papers}></LinkListCard>
+            <TextListCard title="History" items={Contents.Histories}></TextListCard>
+            </div>
         );
     }
 }
