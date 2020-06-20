592d2e0bf4372893ea86dbb021d06a51f5745541
ningenMe <amohkuru@gmail.com>
2020-06-20 16:10:22 +0900

    modify

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React  from 'react';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
-import Space from "./components/Space";
 import './App.css';
 
 function App() {
